86f5bf8ad9b1ca8ac677f4643828fca255b7a43b
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2024-03-13 00:41:41 +0100

    Save pseudocode and  accessability resources

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import styles from './style.css'
+/*
+Your users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+    -use media queries and grid
+- See hover states for all interactive elements on the page
+    -use css to style hove or elements
+- Open a lightbox gallery by clicking on the large product image
+    -WHEN user clicks on large product
+    -DISPLAY lightbox gallery
+
+- Switch the large product image by clicking on the small thumbnail images
+    -WHEN user clicks on the small image
+    -APPLY overlay and border on small image
+    -CHANGE large image to small image
+- Add items to the cart
+    -WHEN user clicks on addToCart
+    -IF numberOfItems is grater than 0
+        -addToCart
+    ELSE display error
+
+
+- View the cart and remove items from it
+    -WHEN users clicks on cart icon
+    -DISPLAY cart content
+    -WHEN users click on bin icon
+    -DELETE itemfrom cart
+*/
